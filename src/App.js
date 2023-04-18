19ec490db050d995412096a442d6d06d48684f69
abeonweb <bmospecific@yahoo.com>
2023-04-18 22:56:50 +0100

    removed location and gallery from route

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import './App.css'
 import { Routes, Route } from "react-router-dom"
 import Layout from "./components/Layout"
 import Home from "./components/Home"
-
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          {/* <Route path="gallery" element={<Gallery />} />
-          <Route path="locations" element={<Locations />} /> */}
+          
         </Route>
       </Routes>
     </div>
