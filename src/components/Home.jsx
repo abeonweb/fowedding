e271ea489f54854b51aa529ace04e5eec8e5f06e
abeonweb <bmospecific@yahoo.com>
2023-04-19 22:23:16 +0100

    import About component to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,15 @@ import styles from "../css/page.module.css";
 import Splash from "./Splash";
 import Gallery from "./Gallery";
 import Locations from "./Locations";
+import About from "./About";
 
 export default function Home() {
   return (
     <main id="home" className={`${styles.home}`}>
+      <div className={styles.overlay}></div>
       <Splash />
       <Gallery />
+      <About />
       <Locations />
     </main>
   );
