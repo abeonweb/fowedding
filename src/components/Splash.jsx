24f1260ef96d8aaabbc0482c5b567267caadedce
abeonweb <bmospecific@yahoo.com>
2023-04-19 22:26:00 +0100

    add methods to water wave added logo image to section

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -5,12 +5,15 @@ import styles from "../css/splash.module.css"
 
 const Splash = () => {
   return (
-    <WaterWave imageUrl={"https://res.cloudinary.com/abeonweb/image/upload/c_scale,w_1200/v1681302015/background_yvr46u.jpg"} 
-    style={{ width: '100%', height: '100%', backgroundSize: 'cover' }}
+    <WaterWave 
+      imageUrl={"https://res.cloudinary.com/abeonweb/image/upload/c_scale,w_1200/v1681302015/background_yvr46u.jpg"} 
+      perturbance={0.03}
+      resolution={256}
+      style={{ width: '100%', height: '100%', backgroundSize: 'cover', backgroundPosition: "25%" }}
     >
       {() => (
-        <section id="splash" className={styles.splashImageContainer}>   
-
+        <section id="splash" className={styles.splashImageContainer}>
+          <img className={styles.logo} src={require("../images/fo_logo.png")} alt=""/>
         </section>
       )}
     </WaterWave>
