53ab1c35988b16d3d816ef6789f31a45eeb127d1
abeonweb <bmospecific@yahoo.com>
2023-04-20 17:52:10 +0100

    moved a tag to Map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "../css/location.module.css";
 
-const Map = ({image}) => {
-  const imageData = require(`../images/${image}.png`)
+const Map = ({ image, link }) => {
+  const imageData = require(`../images/${image}.png`);
   return (
     <div className={styles.map}>
-      <img className={styles.mapImage} src={imageData} alt=""/>
+      <a href={link} target="_blank" rel="noreferrer">
+        <img className={styles.mapImage} src={imageData} alt="" />
+      </a>
     </div>
   );
 };
