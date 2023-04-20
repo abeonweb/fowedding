705ba5e6c4717074cad438fd560a95539a783a9a
abeonweb <bmospecific@yahoo.com>
2023-04-20 17:49:17 +0100

    update css name

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import styles from "../css/page.module.css";
+import styles from "../css/home.module.css";
 import Splash from "./Splash";
 import Gallery from "./Gallery";
 import Locations from "./Locations";
