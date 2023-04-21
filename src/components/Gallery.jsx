e73df4b1db81c785a4d03b111f89dbef65b48d6a
abeonweb <bmospecific@yahoo.com>
2023-04-21 16:29:56 +0100

    add a custom hook to get window width used to apply inline styles

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,9 @@
 import styles from "../css/gallery.module.css";
+import { useEffect, useState } from "react";
 import { photos } from "../data";
 import { Gallery, Item } from "react-photoswipe-gallery";
 import "photoswipe/dist/photoswipe.css";
+import useMediaQuery from "../utils/useMediaQuery";
 
 const photosList = photos.map(({ key, src, width, height }) => {
   const imageSource = require(`../images/${src}.jpg`);
@@ -28,18 +30,23 @@ const photosList = photos.map(({ key, src, width, height }) => {
 });
 
 const PhotoGallery = () => {
+  const matches = useMediaQuery('(min-width: 590px)')
+
+  const galleryStyles = {
+    container: isMobile => ({
+      display: "grid",
+      gridTemplateColumns: isMobile? "171px 220px 171px":"170px 141px",
+      gridTemplateRows: "254px 254px",
+      gridGap: 12,
+    })
+  }
+
   return (
     <section id="gallery" className={`${styles.section}`}>
       <h2 className={styles.sectionTitle}>Some of our photos</h2>
         <Gallery>
           <div
-           className={styles.container}
-           style={{
-              display: "grid",
-              gridTemplateColumns: "240px 171px 171px",
-              gridTemplateRows: "254px 254px",
-              gridGap: 24,
-            }}
+           style={galleryStyles.container(matches)} //pass a boolean to styles obj
           >
             {photosList}
           </div>
