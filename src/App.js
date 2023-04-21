81dd2501398609a0ce59adc7b9d3ed3f0b52f739
abeonweb <bmospecific@yahoo.com>
2023-04-21 16:14:08 +0100

    removed unused class and imported path

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import './App.css'
 import { Routes, Route } from "react-router-dom"
 import Layout from "./components/Layout"
 import Home from "./components/Home"
+
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          
-        </Route>
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+
+      </Route>
+    </Routes>
   );
 }
 
