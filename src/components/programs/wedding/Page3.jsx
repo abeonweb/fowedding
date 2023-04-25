bbe345295acb3255f90f5c3e880cc33edc0e99b5
abeonweb <bmospecific@yahoo.com>
2023-04-25 20:55:12 +0100

    wedding program page 3

---
+++ src/components/programs/wedding/Page3.jsx
@@ -0,0 +1,118 @@
+import styles from "../../../css/program.module.css";
+
+const Page3 = () => (
+  <div className={styles.page}>
+    <p className={styles.subHeading}>BRIDAL PROCESSIONAL HYMN</p>
+    <p className={styles.page3Para}>BH 134: “ALL HAIL THE POWER OF JESUS NAME”</p>
+    <ol className={styles.page3List}>
+      <li className={styles.page3List}>
+        All hail the power of Jesus name! Let Angels prostrate fall; Bring forth
+        the royal diadem, And crown Him Lord of all; Bring forth the royal
+        diadem and crown Him Lord of all,
+      </li>
+      <li className={styles.page3List}>
+        Ye chosen seed of Isreal’s race, Ye ransomed from the father, Hail Him
+        who saves you by his grace, And crown Him, crown Him, crown Him. crown
+        Him Lord of All,
+      </li>
+      <li className={styles.page3List}>
+        Let every kindred, every tribe, on this terrestrial ball, To Him all
+        Majesty ascribe, And crown Him, crown Him, crown Him, crown Him Lord of
+        all,
+      </li>
+      <li className={styles.page3List}>
+        Oh that with yonder sacred throng Weat His feet may fall!&nbsp; We’ll
+        join the everlasting song, And crown Him, crown Him, crown Him, crown
+        Him Lord of all. Amen.
+      </li>
+    </ol>
+
+    <h3 className={styles.subHeading}>CALL TO WORSHIP</h3>
+    <h3 className={styles.subHeading}> OPENING PRAYER</h3>
+    <h3 className={styles.subHeading}>HYMN: YBH 188 “AMAZING GRACE”</h3>
+    
+    <ol className={styles.page3List}>
+      <li className={styles.page3List}>
+        Amazing grace (how sweet the sound) that saved a wretch like me! I was
+        once lost, but now I’m found, was blind, but now I see,
+      </li>
+      <li className={styles.page3List}>
+        ‘Twas grace that taught my heart to fear, and grace my fear relieved;
+        how precious did that grace appear the hour I first believed!
+      </li>
+      <li className={styles.page3List}>Through many dangers, toils and snares I have already come:<br/>
+      ‘tis grace has brought me safe thus far, and grace will lead me home,</li>
+      <li className={styles.page3List}>
+        The Lord has promised good to me, His word my hope secures; He will my
+        shield and portion be as long as life endures,
+      </li>
+      <li className={styles.page3List}>
+        Yes, when this flesh and heart shall fail, and mortal life shall cease;
+        I shall possess, within the veil, a life of joy and peace,
+      </li>
+      <li className={styles.page3List}>
+        The earth shall soon dissolve like snow, the sun forbear to shine; but
+        God, who called me here below, will be forever mine.
+      </li>
+    </ol>
+
+    <h3 className={styles.subHeading}>EXHORTATION</h3>
+    <p className={styles.page3Para}>
+      Dearly beloved, we are gathering together here in the presence of the
+      Almighty God in Christ Jesus by the instrumentality of Holy Spirit, in the
+      presence of His Angels and Ministers of His gospel as well as in the
+      presence of this congregation of His children.
+    </p>
+
+    <p className={styles.page3Para}>
+      We are gathered together to join this woman and this man in Holy
+      Matrimony. Marriage is a holy and honorable state which God Himself
+      founded and established in the Garden of Eden before the coming of sin
+      into human life. Marriage is an estate of utmost importance and therefore
+      must be treated with dignity and reverence.
+    </p>
+
+    <p className={styles.page3Para}>
+      Jesus our Lord and Savior bestowed honor, reverence, dignity and His
+      blessings on marriage by His personal presence at the wedding in Cana of
+      Galilee as well as performing His first miracle there. The scriptures
+      compare the union of husband and wife with the Holy and Mystic union which
+      exists between Christ and His Church.
+    </p>
+
+    <p className={styles.page3Para}>
+      That is these two, husband and wife will leave all others and cleave to
+      each other becoming one flesh. In unity of soul and body, both will leave
+      together as an inseparable pair, and helper fit for each other, and pray
+      together as join theirs of the grace of eternal life in Christ Jesus, duly
+      considering the causes for which marriage was ordained.
+    </p>
+
+    <p className={styles.page3Para}>
+      Firstly, It was ordained for mutual societal help and comfort that one
+      ought to have for each other.
+    </p>
+
+    <p className={styles.page3Para}>
+      Secondly, It was ordained in order that the natural instincts and
+      affections implanted by God to this holy estate should continue there in
+      pureness of living.
+    </p>
+
+    <p className={styles.page3Para}>
+      Thirdly, I was ordained for the increase of mankind according to the will
+      of God and that children might be brought up in His fear and the nature of
+      the Lord, and to the praise of His name.
+    </p>
+
+    <p className={styles.page3Para}>
+      These people have come to be joined together into the holy estate of
+      matrimony. Therefore if anyone among the congregation present here today
+      knows of any just cause why they should not be joined together according
+      to the word of God and the laws of the Land, let such a person say it now
+      or forever hold his or her PEACE.
+    </p>
+  </div>
+);
+
+export default Page3;
