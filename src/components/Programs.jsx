ac7b84a9b5232a9876dfa03a79fb043953282f2a
abeonweb <bmospecific@yahoo.com>
2023-04-27 23:16:20 +0100

    change to nav bottom value

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -11,6 +11,19 @@ import EngagementProgram from "./EngagementProgram";
 import { programs } from "../data";
 import styles from "../css/program.module.css"
 
+const blue = {
+  50: "#F0F7FF",
+  100: "#C2E0FF",
+  200: "#80BFFF",
+  300: "#66B2FF",
+  400: "#3399FF",
+  500: "#007FFF",
+  600: "#0072E5",
+  700: "#0059B2",
+  800: "#004C99",
+  900: "#003A75",
+};
+
 const grey = {
   50: "#f6f8fa",
   100: "#eaeef2",
