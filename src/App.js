8df1c7e0d663e2a7c40d217fb16f9da255716fb6
abeonweb <bmospecific@yahoo.com>
2023-04-27 08:33:11 +0100

    useparams to scroll to section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { Routes, Route } from "react-router-dom"
 import Layout from "./components/Layout"
 import Home from "./components/Home"
+import Program from "./components/Program"
+
 
 function App() {
   return (
+
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-
+        <Route path="/:id" element={<Home />} />
+        <Route path="program" element={<Program />} />
       </Route>
     </Routes>
+
   );
 }
 
