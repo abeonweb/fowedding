4dd0accc5ef2256f0c6cd074fda7bcd804d7b45b
abeonweb <bmospecific@yahoo.com>
2023-04-27 18:17:47 +0100

    changed to wedding program

--- src/components/WeddingProgram.jsx
+++ src/components/WeddingProgram.jsx
@@ -1,5 +1,3 @@
-import React, {useEffect} from "react";
-import {scrollIntoTheView} from "../utils/scroll"
 import styles from "../css/program.module.css";
 import Page1 from "./programs/wedding/Page1";
 import Page2 from "./programs/wedding/Page2";
@@ -8,12 +6,9 @@ import Page4 from "./programs/wedding/Page4";
 import Page5 from "./programs/wedding/Page5";
 import Page6 from "./programs/wedding/Page6";
 
-const Program = () => {
-  useEffect(()=>{
-    scrollIntoTheView("program")
-  },[])
+const WeddingProgram = () => {
   return (
-    <div id="program" className={styles.program}>
+    <div className={styles.program}>
       <Page1 />
       <Page2 />
       <Page3 />
@@ -25,4 +20,4 @@ const Program = () => {
   );
 };
 
-export default Program;
+export default WeddingProgram;
