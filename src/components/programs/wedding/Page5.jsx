7cb42b603d7cbe5941f9fb9b4372062377e0cab0
abeonweb <bmospecific@yahoo.com>
2023-04-27 05:36:40 +0100

    wedding program page 5

---
+++ src/components/programs/wedding/Page5.jsx
@@ -0,0 +1,91 @@
+import styles from "../../../css/program.module.css";
+
+const Page5 = () => {
+  return (
+    <div className={styles.page}>
+      <h2 className={styles.subHeading}>8. JOINING AND BLESSING</h2>
+      <p className={styles.page5joining}>
+        <span className={styles.highlightSpan}>Minister:</span> Now that your family members, relatives, friends and Christian
+        brethren have with one voice pledged to support you with their prayers.
+        It is time to take your wedding vows.
+      </p>
+      <p className={styles.page5joining}>
+        <span className={styles.highlightSpan}>Groom:</span> I, <span className={styles.italicHighlightSpan}>Oluwole Olusola Fagbemi</span>, take you <span className={styles.italicHighlightSpan}>Folake Olawunmi Oke</span>, as my
+        lawful wedded wife from today on and henceforth. To have and to hold, in
+        health and in sickness, in joy ad in sorrow, in prosperity and in
+        adversity. I will always love you with all my worldly goods which God
+        provides for as long as we both live. May the Lord bless our union to
+        the glory of His name. Amen.
+      </p>
+      <p className={styles.page5joining}>
+        <span className={styles.highlightSpan}>Bride:</span> I, <span className={styles.italicHighlightSpan}>Folake Olawunmi Oke</span>, accept you <span className={styles.italicHighlightSpan}>Oluwole Olusola Fagbemi</span>, as my
+        lawful wedded husband from today on and henceforth. To have and to hold,
+        in health and in sickness, in joy and in sorrow, in prosperity and in
+        adversity. I will always love you, I will care for you with all my
+        worldly goods which God provides for me as long as we both live. May God
+        bless our union to the glory of His name. Amen.
+      </p>
+      <h3 className={styles.subHeading2}>WEDDING BIBLE</h3>
+      <p className={styles.page5joining}>
+        Minister: <span className={styles.italicHighlightSpan}>Oluwole Olusola Fagbemi</span>, what is the symbol of truth which you
+        have brought for your bride?
+      </p>
+      <p className={styles.page5joining}>
+        <span className={styles.highlightSpan}>Minister:</span> The Bible is God’s revelation to mankind. It is God’s guide
+        and pathfinder to His son, Jesus Christ to eternal life. May God help
+        you build your home on the Lord Jesus Christ who is revealed in this
+        book as the way, the truth and the life. Amen.
+      </p>
+      <p className={styles.page5joining}>
+        <span className={styles.highlightSpan}>Groom:</span> I give you this bible as the symbol of Truth to you. May the Lord
+        help me to follow the precepts therein in the direction of our home.
+        Amen.
+      </p>
+      <p className={styles.page5joining}>
+        <span className={styles.highlightSpan}>Bride:</span> I gladly accept this Bible as a symbol of your truth to me and my
+        truth to which I enter into union with you today. May the Lord help me
+        to be truthful to you as the Head of our home as we both follow this
+        book which leads to the truth in Christ. Amen.
+      </p>
+
+      <h3 className={styles.subHeading2}>WEDDING RINGS</h3>
+      <p className={styles.page5joining}>
+        <span className={styles.highlightSpan}>Minister:</span> <span className={styles.italicHighlightSpan}>Folake Olawunmi</span> and <span className={styles.italicHighlightSpan}>Oluwole Olusola</span>, what did you bring as
+        symbol of love for each other?
+      </p>
+      <p className={styles.page5joining}>
+        <span className={styles.highlightSpan}>Minister:</span> These rings symbolize eternal love which knows no beginning
+        and no ending. The materials with which the rings are made remind us of
+        what never rusts or decays because of its purity. May the Lord help you
+        both to keep your love for each other pure and eternal. Amen.
+      </p>
+      <p className={styles.page5joining}>
+        <span className={styles.highlightSpan}>Groom:</span> This ring is a symbol and token of my love which I have for you
+        as my wife. With my body, I honor you. All that I am and all that I have
+        I will use to care for you in the name of the Father, the Son and of the
+        Holy spirit.
+      </p>
+      <p className={styles.page5joining}>
+        <span className={styles.highlightSpan}>Bride:</span> This ring is a symbol and a token of my love which I have for you
+        as my husband. With my body, I honor you. All that I am and all that I
+        have I will use to care for you in the name of the Father, the Son, and
+        the Holy spirit. Amen.
+      </p>
+      <h3 className={styles.subHeading2}>PRONOUNCEMENT</h3>
+      <p className={styles.page5joining}>
+        In as much as <span className={styles.italicHighlightSpan}>Oluwole Olusola Fagbemi</span> and <span className={styles.italicHighlightSpan}>Folake Olawunmi Oke</span> have been
+        united in Holy Matrimony, the testimony of which the ministers of the
+        gospel of Christ and the entire congregation witnessed. In as much as
+        they have also demonstrated this is the exchange of vows and rings,
+        according to the privilege and anointing which I have as a minister of
+        the gospel of Jesus Christ and according to the Marriage Act of the
+        Federal Republic Of Nigeria. I pronounce you Husband and Wife <span className={styles.italicHighlightSpan}>(Mr. and
+        Mrs. Oluwole Olusola Fagbemi).</span> “Whoever God has put together, let no man
+        put asunder”.
+      </p>
+      <p className={`${styles.page5joining} ${styles.highlightSpan}`}>Let us Pray</p>
+    </div>
+  );
+};
+
+export default Page5;
