d411840634d0ea46bd653482d1425f5a0eeb1781
abeonweb <bmospecific@yahoo.com>
2023-04-27 22:44:39 +0100

    remove font weight style

--- src/components/programs/engagement/Page7.jsx
+++ src/components/programs/engagement/Page7.jsx
@@ -5,15 +5,15 @@ const Page1 = () => {
   return (
     <div className={styles.page}>
       <p className={styles.subHeading2}>OUR SINCERE GRATITUDE</p>
-      <p className={`${styles.page1Para} ${styles.darker}`}>
+      <p className={styles.page1Para}>
         To the Almighty ancient of days who has never left our side. The one who
         no man can question and who has made this beautiful moment possible.
       </p>
-      <p className={`${styles.page1Para} ${styles.darker}`}>
+      <p className={styles.page1Para}>
         To our parents for their love, support, counselling and prayers and to
         all our friends and well-wishers for sharing in our joy.
       </p>
-      <p className={`${styles.page1Para} ${styles.darker}`}>
+      <p className={styles.page1Para}>
         May the Almighty God reward you abundantly, God bless you all.
       </p>
       <p className={`${styles.page1Para} ${styles.smaller}`}>
