912489d9e8103535f61ecfc249ac87c1e18de6d9
abeonweb <bmospecific@yahoo.com>
2023-04-28 09:51:20 +0100

    use props with the func in  useEffect removed unwanted components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,20 @@
+import { useEffect} from "react";
 import styles from "../css/home.module.css";
 import Splash from "./Splash";
-import Gallery from "./Gallery";
-import Locations from "./Locations";
 import About from "./About";
+import { scrollIntoTheView } from "../utils/scroll";
 
-export default function Home() {
+
+export default function Home({section}) {
+
+  useEffect(() => {
+    scrollIntoTheView(section);
+  }, [section]);
+
   return (
     <main id="home" className={`${styles.home}`}>
-      <div className={styles.overlay}></div>
       <Splash />
-      <Gallery />
-      <About />
-      <Locations />
+      <About/>
     </main>
   );
 }
