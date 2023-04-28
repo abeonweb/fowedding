488b1c3d74def06b401a5cebcb17a6817413ffad
abeonweb <bmospecific@yahoo.com>
2023-04-28 09:43:21 +0100

    added state to control section change on home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import { useState } from "react"
 import { Routes, Route } from "react-router-dom"
 import Layout from "./components/Layout"
 import Home from "./components/Home"
+import Gallery from "./components/Gallery"
+import Programs from "./components/Programs"
+import Locations from "./components/Locations"
 
 
 function App() {
+  const [section, setSection] = useState("home")
   return (
 
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/:id" element={<Home />} />
+      <Route path="/" element={<Layout setSection={setSection}/>}>
+        <Route index element={<Home section={section}/>} />
+        <Route path="gallery" element={<Gallery />} />
+        <Route path="locations" element={<Locations />} />
+        <Route path="programs" element={<Programs />} />
       </Route>
     </Routes>
 
