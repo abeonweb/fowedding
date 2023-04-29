1df7545dc69a60e4370fe8acb6ff746249be603f
abeonweb <bmospecific@yahoo.com>
2023-04-29 21:13:19 +0100

    Gifts component created

---
+++ src/components/Gifts.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "../css/gifts.module.css"
+const Gifts = () => {
+  return (
+    <section>
+        <h1>Gifts and Cash Gifts</h1>
+      <p className={styles.message}>
+        Like most couples who have known each other for a while, we’re
+        surrounded by all the household treasures anyone could ever want or
+        need. No gifts are ever required, but if you wish to give, you’ll find
+        our [[[[[registry details at [link]. ]]]]]
+      </p>
+
+      <article>
+        <h2>Cash Gifts</h2>
+        <ul>
+          <li>
+            Please send all cash gifts to Folake Olawunmi Oke 3019641561 Polaris
+            bank
+          </li>
+          <li>
+            For US and UK cash gifts Paypal account info: Folake Oke Email:
+            fola.o.oke@gmail.com
+          </li>
+        </ul>
+      </article>
+    </section>
+  );
+};
+
+export default Gifts;
