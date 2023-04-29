587877ce25c051920bac561cf3acf5ce8f86fd8f
abeonweb <bmospecific@yahoo.com>
2023-04-29 21:41:18 +0100

    classes added

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -5,21 +5,21 @@ const Information = () => {
     <div id="info" className={styles.section}>
       <h1 className={styles.sectionTitle}>Information of Interest</h1>
       <article>
-        <h2>Hotels</h2>
-        <p>
+        <h2 className={styles.articleTitle}>Hotels</h2>
+        <p className={styles.message}>
           For those of our friends and family that are coming from out of town,
           here is a list of hotels. If you have not already booked, try to do so
           in advance.
         </p>
-        <ul>
-          <li>Watercress hotel</li>
-          <li>Safron hotel</li>
-          <li>Derembrandt hotel</li>
-          <li>Ibis hotel ikeja</li>
-          <li>Ivy hotel Adeniyi Jones</li>
-          <li>Lagos Marriott hotel</li>
-          <li>Radisson blue hotel</li>
-          <li>Protea hotel</li>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Watercress hotel</li>
+          <li className={styles.listItem}>Safron hotel</li>
+          <li className={styles.listItem}>Derembrandt hotel</li>
+          <li className={styles.listItem}>Ibis hotel ikeja</li>
+          <li className={styles.listItem}>Ivy hotel Adeniyi Jones</li>
+          <li className={styles.listItem}>Lagos Marriott hotel</li>
+          <li className={styles.listItem}>Radisson blue hotel</li>
+          <li className={styles.listItem}>Protea hotel</li>
         </ul>
       </article>
     </div>
