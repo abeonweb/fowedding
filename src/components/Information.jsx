88a0e8aea170567153a899bda1c61542be3cf600
abeonweb <bmospecific@yahoo.com>
2023-04-29 21:14:08 +0100

    program and location moved, hotel info added

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,13 +1,27 @@
 import styles from "../css/info.module.css";
-import Locations from "./Locations";
-import Programs from "./Programs";
 
 const Information = () => {
   return (
     <div id="info" className={styles.section}>
       <h1 className={styles.sectionTitle}>Information of Interest</h1>
-      <Locations />
-      <Programs />
+      <article>
+        <h2>Hotels</h2>
+        <p>
+          For those of our friends and family that are coming from out of town,
+          here is a list of hotels. If you have not already booked, try to do so
+          in advance.
+        </p>
+        <ul>
+          <li>Watercress hotel</li>
+          <li>Safron hotel</li>
+          <li>Derembrandt hotel</li>
+          <li>Ibis hotel ikeja</li>
+          <li>Ivy hotel Adeniyi Jones</li>
+          <li>Lagos Marriott hotel</li>
+          <li>Radisson blue hotel</li>
+          <li>Protea hotel</li>
+        </ul>
+      </article>
     </div>
   );
 };
