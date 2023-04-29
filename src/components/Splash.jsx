e4bc1467cef74c74dcd9b76660413ed2fa8c8bbf
abeonweb <bmospecific@yahoo.com>
2023-04-29 20:01:38 +0100

    increased perturbance

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -6,7 +6,7 @@ const Splash = () => {
   return (
     <WaterWave 
       imageUrl={"https://res.cloudinary.com/abeonweb/image/upload/v1682182063/blackslab_n2blsh.jpg"} 
-      perturbance={0.008}
+      perturbance={0.025}
       resolution={256}
       style={{ width: '100%', height: '100%', backgroundSize: 'cover', backgroundPosition: "25%" }}
     >
