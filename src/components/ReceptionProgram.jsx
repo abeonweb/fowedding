102f12f10c12def7cf6fa2b7a06be8b38baee4eb
abeonweb <bmospecific@yahoo.com>
2023-04-29 20:01:23 +0100

    page 2 removed

--- src/components/ReceptionProgram.jsx
+++ src/components/ReceptionProgram.jsx
@@ -1,12 +1,11 @@
 import styles from "../css/program.module.css";
 import Page1 from "./programs/reception/Page1";
-import Page2 from "./programs/reception/Page2";
+
 
 const ReceptionProgram = () => {
   return (
     <div className={styles.program}>
         <Page1/>
-        <Page2/>
     </div>
   )
 }
