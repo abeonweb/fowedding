bbed5b377726ad8e9521f135731f7aec3ac1b828
abeonweb <bmospecific@yahoo.com>
2023-04-30 13:01:59 +0100

    add portfolio link to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,8 @@ import styles from '../css/navigation.module.css'
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-        <p className={styles.textFooter}>&copy; Folake &amp; Oluwole 2023</p>
+        <p className={styles.copyright}>&copy; Folake &amp; Oluwole 2023</p>
+        <p className={styles.creator}><a href="http://abecodes.com">Created by: <span className={styles.portfolioLink}>abecodes.com</span></a></p>
     </footer>
   )
 }
